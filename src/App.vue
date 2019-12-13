37ce765168b69bdd5608d413ee7e37995a8cee0e
Semih Arslan <semihsemih@me.com>
2019-12-13 15:16:56 +0300

    template init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="header mt-2 mb-5">
+      <h1>Blackjack Vue</h1>
+    </div>
+    <div class="game-actions row mb-3 w-75">
+        <div class="col-6 buttons">
+          <button class="btn btn-info">New Game</button>
+        </div>
+        <div class="col-6 infos text-right">
+          <p class="credit">Credit: $1000</p>
+        </div>
+    </div>
+    <GameArea />
+  </div>
+</template>
+
+<script>
+  import GameArea from "./components/GameArea";
+export default {
+  name: "app",
+  components: {
+    GameArea
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/GameArea.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="game-area">
+    <div class="croupier-area">
+      <div class="game-cards">
+        <div class="item"><img src="../assets/img/cards/2C.png" alt=""></div>
+        <div class="item"><img src="../assets/img/cards/7D.png" alt=""></div>
+        <div class="item"><img src="../assets/img/cards/3S.png" alt=""></div>
+        <div class="item"><img src="../assets/img/cards/5H.png" alt=""></div>
+      </div>
+    </div>
+    <div class="player-area"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GameArea"
+  }
+</script>
+
+<style scoped>
+
+</style>
